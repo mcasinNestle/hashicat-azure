81bbd14d19c47fa62c60c0992b4b8a11424f5bbb
mcasinNestle <123961362+mcasinNestle@users.noreply.github.com>
2023-04-20 10:31:55 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Outputs file
-output "catapp_url" {
-  value = "http://${azurerm_public_ip.catapp-pip.fqdn}"
-}
+#output "catapp_url" {
+#  value = "http://${azurerm_public_ip.catapp-pip.fqdn}"
+#}
 
-output "catapp_ip" {
-  value = "http://${azurerm_public_ip.catapp-pip.ip_address}"
-}
+#output "catapp_ip" {
+#  value = "http://${azurerm_public_ip.catapp-pip.ip_address}"
+#}
